0b40276a1f0b5975d4f599552e0a92da1f5f84cf
DESKTOP-JRJFEUM\jsy <sju01181@naver.com>
2022-12-31 14:34:15 +0900

    Hilt init-{ 1. Dependencies 2. MvvmSampleApplication.kt - @HiltAndroidApp 3. MainActivity.kt - @AndroidEntryPoint 4. MainViewModel.kt - @HiltViewModel,  @Inject constructor }

--- app/src/main/java/jsy/test/mvvmsample/ui/main/MainViewModel.kt
+++ app/src/main/java/jsy/test/mvvmsample/ui/main/MainViewModel.kt
@@ -3,8 +3,11 @@ package jsy.test.mvvmsample.ui.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
 
     private val _mainText = MutableLiveData<String>()
     val mainText: LiveData<String>
