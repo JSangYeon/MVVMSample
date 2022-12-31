45eaa026b921fe8e66fb43a4a8598805d2bfb1b2
DESKTOP-JRJFEUM\jsy <sju01181@naver.com>
2022-12-31 14:58:46 +0900

    1. strings.xml - testProvider 추가 2. MainViewModel.kt - 생성자 ResourceProvider.kt 추가 3. ResourceProvider.kt 추가 (for getResource in viewModel)

--- app/src/main/java/jsy/test/mvvmsample/ui/main/MainViewModel.kt
+++ app/src/main/java/jsy/test/mvvmsample/ui/main/MainViewModel.kt
@@ -4,21 +4,24 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import jsy.test.mvvmsample.R
+import jsy.test.mvvmsample.model.repository.ResourcesProvider
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(
+    private val resourcesProvider: ResourcesProvider
+) : ViewModel() {
 
     private val _mainText = MutableLiveData<String>()
     val mainText: LiveData<String>
         get() = _mainText
-
 
     init {
         _mainText.value = "1234"
     }
 
     fun changeMainText(){
-        _mainText.value = "5555"
+        _mainText.value = resourcesProvider.getString(R.string.test_provider)
     }
 }
